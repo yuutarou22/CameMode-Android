8c1c5417f6e6f5e7bc639b0dc6f5ce9984ebbd93
YutaroM <yutaroapp@gmail.com>
2020-06-14 15:29:24 +0900

    検索結果フラグメントの追加

---
+++ app/src/main/java/com/example/camemode/Fragment/Search/SearchResultFragment.kt
@@ -0,0 +1,32 @@
+package com.example.camemode.Fragment.Search
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.camemode.Fragment.BaseFragment
+
+import com.example.camemode.R
+
+class SearchResultFragment : BaseFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search_result, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+}
