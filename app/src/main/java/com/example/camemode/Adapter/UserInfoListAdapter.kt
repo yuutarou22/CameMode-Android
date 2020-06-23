588ae26de6e3b5f5b409af18a468271faa236df6
YutaroM <yutaroapp@gmail.com>
2020-06-24 07:19:37 +0900

    ユーザ情報を表示するRecyclerViewのAdapterを作成

---
+++ app/src/main/java/com/example/camemode/Adapter/UserInfoListAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.camemode.Adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+public class UserInfoListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
