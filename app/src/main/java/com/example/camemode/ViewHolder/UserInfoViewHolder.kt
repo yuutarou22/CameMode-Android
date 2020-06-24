67cd2fe26ca95dc1e9af25afaa94d812b9b325c9
YutaroM <yutaroapp@gmail.com>
2020-06-25 08:40:15 +0900

    ユーザ情報を保持するViewHolderの作成

---
+++ app/src/main/java/com/example/camemode/ViewHolder/UserInfoViewHolder.kt
@@ -0,0 +1,11 @@
+package com.example.camemode.ViewHolder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+public class UserInfoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    public fun UserInfoViewHolder(itemView: View) {
+
+    }
+}
