253524c987d3df8745e8b974460ce9b7fb696bd9
YutaroM <yutaroapp@gmail.com>
2020-06-28 19:20:44 +0900

    UserInfoViewHolderを修正（本来の目的であるレイアウトの情報を保持するために特化）

--- app/src/main/java/com/example/camemode/ViewHolder/UserInfoViewHolder.kt
+++ app/src/main/java/com/example/camemode/ViewHolder/UserInfoViewHolder.kt
@@ -1,11 +1,20 @@
 package com.example.camemode.ViewHolder
 
 import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.camemode.R
 
-public class UserInfoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class UserInfoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    public fun UserInfoViewHolder(itemView: View) {
-
-    }
+    val mLineView : View = itemView.findViewById(R.id.line_view)
+    val mUserIcon : ImageView = itemView.findViewById(R.id.user_icon)
+    val mUserName : TextView = itemView.findViewById(R.id.user_name)
+    val mSnsTranslationButton : ImageButton = itemView.findViewById(R.id.sns_transition)
+    val mCategoryRole : TextView = itemView.findViewById(R.id.category_role)
+    val mImaginationHope : TextView = itemView.findViewById(R.id.imagination_hope)
+    val mAge : TextView = itemView.findViewById(R.id.age)
+    val mSex : TextView = itemView.findViewById(R.id.sex)
 }
