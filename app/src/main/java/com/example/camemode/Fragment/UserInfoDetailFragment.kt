5a74fd7abe831051512aebae816f83e23924adb7
YutaroM <yutaroapp@gmail.com>
2020-07-06 08:45:32 +0900

    ユーザ詳細Fragmentの追加

---
+++ app/src/main/java/com/example/camemode/Fragment/UserInfoDetailFragment.kt
@@ -0,0 +1,44 @@
+package com.example.camemode.Fragment
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.camemode.R
+import kotlinx.android.synthetic.main.fragment_user_info_detail.*
+
+class UserInfoDetailFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_info_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+    private fun initView() {
+        back_button.setOnClickListener {
+            fragmentManager?.popBackStack()
+        }
+    }
+}
