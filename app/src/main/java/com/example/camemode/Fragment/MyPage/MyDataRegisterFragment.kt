f077f10e394629b3f05d0be1131b9539aa05d6c3
YutaroM <yutaroapp@gmail.com>
2020-07-30 08:01:13 +0900

    マイデータ登録画面 内部処理の大枠を作成

--- app/src/main/java/com/example/camemode/Fragment/MyPage/MyDataRegisterFragment.kt
+++ app/src/main/java/com/example/camemode/Fragment/MyPage/MyDataRegisterFragment.kt
@@ -2,12 +2,15 @@ package com.example.camemode.Fragment.MyPage
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.camemode.Fragment.BaseFragment
 
 import com.example.camemode.R
+import kotlinx.android.synthetic.main.fragment_my_data_register.*
 
 class MyDataRegisterFragment : BaseFragment() {
 
@@ -23,11 +26,36 @@ class MyDataRegisterFragment : BaseFragment() {
         return inflater.inflate(R.layout.fragment_my_data_register, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
     }
 
     override fun onDetach() {
         super.onDetach()
+    }
+
+    private fun initView() {
+        imagination_hope.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+             }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+        })
+
+        regist_button.setOnClickListener {
+            // ToDo: 入力内容の確認ダイアログを表示
+            // ToDo: バリデーションチェック処理
+            // ToDo: NCMBへの登録処理
+        }
     }
 }
