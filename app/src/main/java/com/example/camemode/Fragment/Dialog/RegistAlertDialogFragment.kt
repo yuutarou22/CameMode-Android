63ff37bba532f76d44954e70c19d81d5bc469710
YutaroM <yutaroapp@gmail.com>
2020-08-02 15:27:41 +0900

    登録確認ダイアログの実装

--- app/src/main/java/com/example/camemode/Fragment/Dialog/RegistAlertDialogFragment.kt
+++ app/src/main/java/com/example/camemode/Fragment/Dialog/RegistAlertDialogFragment.kt
@@ -1,12 +1,30 @@
 package com.example.camemode.Fragment.Dialog
 
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.example.camemode.R
 
-class RegistAlertDialogFragment : DialogFragment() {
+class RegistAlertDialogFragment : DialogFragment(){
+
+    private var mListener: DialogOkClickListener? = null
+
+    interface DialogOkClickListener {
+        fun onOkClicked(dialog: DialogFragment)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        try {
+            mListener = this.targetFragment as DialogOkClickListener
+        } catch (e: ClassCastException) {
+            throw ClassCastException((context.toString() +
+                    " must implement DialogOkClickListener"))
+        }
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
@@ -16,13 +34,10 @@ class RegistAlertDialogFragment : DialogFragment() {
             builder.setTitle(getString(R.string.regist_alert_dialog_confirm_title))
                 .setMessage(R.string.regist_alert_dialog_confirm_message)
                 .setNeutralButton("はい", { dialog, which ->
-                    // ToDo: バリデーションチェック処理
-                    
                     // ToDo: NCMBへの登録処理
+                    mListener?.onOkClicked(this)
                 })
-                .setPositiveButton("いいえ", { dialog, which ->
-
-                })
+                .setPositiveButton("いいえ", { dialog, which -> })
 
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be nunll")
