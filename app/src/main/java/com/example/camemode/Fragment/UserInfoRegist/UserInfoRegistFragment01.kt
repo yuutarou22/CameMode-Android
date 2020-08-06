8aafba9aece7c74d72a7e01fc59a8d6fcc9e4381
YutaroM <yutaroapp@gmail.com>
2020-08-07 07:04:52 +0900

    ４画面のフラグメントを追加 ・暫定で背景色のみ指定

---
+++ app/src/main/java/com/example/camemode/Fragment/UserInfoRegist/UserInfoRegistFragment01.kt
@@ -0,0 +1,22 @@
+package com.example.camemode.Fragment.UserInfoRegist
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.camemode.R
+
+class UserInfoRegistFragment01 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_user_info_regist_01, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/camemode/Fragment/UserInfoRegist/UserInfoRegistFragment03.kt
@@ -0,0 +1,20 @@
+package com.example.camemode.Fragment.UserInfoRegist
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.camemode.R
+
+class UserInfoRegistFragment03 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_user_info_regist_03, container, false)
+    }
+}
