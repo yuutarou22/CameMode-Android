35005a33ce9fef277eb5578ecb464e56c94063ed
YutaroM <yutaroapp@gmail.com>
2020-08-15 06:40:04 +0900

    ・ユーザ情報登録時、ローカル保持していた値を表示する ・レイアウトはユーザ情報詳細画面を流用（fragment_user_info_detail） ・編集ボタンを設置し、ユーザ照会画面のみ表示するよう改修（edit_button） ・表示する文字列の判定処理を追加 （ユーザ情報詳細画面と同じで冗長なため共通化の必要あり）

--- app/src/main/java/com/example/camemode/Fragment/MyPage/MyDataShowFragment.kt
+++ app/src/main/java/com/example/camemode/Fragment/MyPage/MyDataShowFragment.kt
@@ -1,18 +1,36 @@
 package com.example.camemode.Fragment.MyPage
 
 import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.camemode.Fragment.BaseFragment
+import com.example.camemode.Model.UserInfo
 
 import com.example.camemode.R
+import kotlinx.android.synthetic.main.content_user_info_age.*
+import kotlinx.android.synthetic.main.content_user_info_charge.*
+import kotlinx.android.synthetic.main.content_user_info_photo_image.*
+import kotlinx.android.synthetic.main.content_user_info_region.*
+import kotlinx.android.synthetic.main.content_user_info_sex.*
+import kotlinx.android.synthetic.main.fragment_user_info_detail.*
 
 class MyDataShowFragment : BaseFragment() {
 
+    /**
+     * ローカル保持用
+     */
+    lateinit var data : SharedPreferences
+    lateinit var editor : SharedPreferences.Editor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        data = context!!.getSharedPreferences("UserInfoData", Context.MODE_PRIVATE)
     }
 
     override fun onCreateView(
@@ -20,7 +38,37 @@ class MyDataShowFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_data_show, container, false)
+        return inflater.inflate(R.layout.fragment_user_info_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+
+        region.text = getRegion(data.getInt(UserInfo.FIELD_REGION, 1))
+        category_role_icon.setImageResource(getUserIcon(data.getInt(UserInfo.FIELD_CATEGORY_ROLE, 1)))
+        category_role_icon.setBackgroundColor(getUserIconBg(data.getInt(UserInfo.FIELD_CATEGORY_ROLE, 1)))
+        category_role_text.text = getCategoryRole(data.getInt(UserInfo.FIELD_CATEGORY_ROLE, 1))
+        charge.text = getCharge(data.getInt(UserInfo.FIELD_CHARGE, 1))
+        age.text = getAge(data.getInt(UserInfo.FIELD_AGE, 1))
+        display_name.text = data.getString(UserInfo.FIELD_DISPLAY_NAME, "")
+        photo_image.text = data.getString(UserInfo.FIELD_PHOTO_IMAGE, "")
+        sex.text = getSex(data.getInt(UserInfo.FIELD_SEX, 1))
+
+        edit_bubtton.visibility = View.VISIBLE
+
+        back_button.setOnClickListener {
+            fragmentManager?.popBackStack()
+        }
+
+        sns_transition_button.setOnClickListener {
+            val uri: Uri = Uri.parse(context?.resources?.getString(R.string.twitter_url) + data.getString(UserInfo.FIELD_TWITTER_ID, "") + "/")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 
     override fun onAttach(context: Context) {
@@ -29,5 +77,106 @@ class MyDataShowFragment : BaseFragment() {
 
     override fun onDetach() {
         super.onDetach()
+    }
+    /**
+     * カテゴリ種別を文字列に変換
+     * @param categoryRole
+     * @return String
+     */
+    private fun getCategoryRole(categoryRole: Int?): String {
+        return when(categoryRole) {
+            0 -> getString(R.string.category_role_camera)
+            1 -> getString(R.string.category_role_model)
+            else -> getString(R.string.category_role_camera_model)
+        }
+    }
+
+    /**
+     * カテゴリ種別によってアイコン画像を切り替え
+     * @param categoryRole
+     * @return Int
+     */
+    private fun getUserIcon(categoryRole: Int?): Int {
+        return when(categoryRole) {
+            0 -> R.drawable.ic_user_info_camera
+            1 -> R.drawable.ic_user_info_model
+            else -> R.drawable.ic_user_info_camera_model
+        }
+    }
+
+    /**
+     * カテゴリ種別によってアイコン画像の背景色を切り替え
+     * @param categoryRole
+     * @return Int
+     */
+    private fun getUserIconBg(categoryRole: Int?): Int {
+        return when(categoryRole) {
+            0 -> Color.argb(255,255,177,208)
+            1 -> Color.argb(255,189,211,255)
+            else -> Color.argb(255,150,240,180)
+        }
+    }
+
+    /**
+     * 有償・無償
+     * @param charge
+     * @return String
+     */
+    private fun getCharge(charge: Int?): String {
+        return when(charge) {
+            0 -> getString(R.string.charge)
+            else -> getString(R.string.charge_not)
+        }
+    }
+
+    /**
+     * 年代
+     * @param age
+     * @return String
+     */
+    private fun getAge(age: Int?): String {
+        return when(age) {
+            0 -> getString(R.string.age_unselected)
+            1 -> getString(R.string.age_10)
+            2 -> getString(R.string.age_20)
+            3 -> getString(R.string.age_30)
+            4 -> getString(R.string.age_40)
+            5 -> getString(R.string.age_50)
+            else -> getString(R.string.age_60_adove)
+        }
+    }
+
+    /**
+     * 性別
+     * @param sex
+     * @return String
+     */
+    private fun getSex(sex: Int?): String {
+        return when(sex) {
+            0 -> getString(R.string.sex_not_select)
+            1 -> getString(R.string.sex_man)
+            else -> getString(R.string.sex_woman)
+        }
+    }
+
+    /**
+     * 地域
+     * @param region
+     * @return String
+     */
+    private fun getRegion(region: Int?): String {
+        return when(region) {
+            0 -> getString(R.string.region_hokkaido)
+            1 -> getString(R.string.region_tohoku)
+            2 -> getString(R.string.region_kanto)
+            3 -> getString(R.string.region_hokuriku)
+            4 -> getString(R.string.region_chubu)
+            5 -> getString(R.string.region_kinki)
+            6 -> getString(R.string.region_shikoku)
+            7 -> getString(R.string.region_chugoku)
+            8 -> getString(R.string.region_kyusyu)
+            9 -> getString(R.string.region_okinawa)
+            else -> getString(R.string.region_not_select)
+        }
     }
 }
