7b82b09067c9279deb8974d4fe5c26640e54dbf0
YutaroM <yutaroapp@gmail.com>
2020-08-25 07:01:46 +0900

    ・ユーザ情報編集画面を開いた時、SharedPreferencesの情報を表示する処理を追加 （ラジオボタンに関しては、SharedPreferencesの値を元にチェックするIDを指定する）

--- app/src/main/java/com/example/camemode/Fragment/MyPage/MyDataEditFragment.kt
+++ app/src/main/java/com/example/camemode/Fragment/MyPage/MyDataEditFragment.kt
@@ -1,6 +1,7 @@
 package com.example.camemode.Fragment.MyPage
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,9 +11,15 @@ import com.example.camemode.Fragment.BaseFragment
 import com.example.camemode.R
 
 class MyDataEditFragment : BaseFragment() {
+    /**
+     * ローカル保持用
+     */
+    lateinit var data : SharedPreferences
+    lateinit var editor : SharedPreferences.Editor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        data = context!!.getSharedPreferences("UserInfoData", Context.MODE_PRIVATE)
     }
 
     override fun onCreateView(
@@ -23,11 +30,46 @@ class MyDataEditFragment : BaseFragment() {
         return inflater.inflate(R.layout.fragment_my_data_edit, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        category_role.check(getCheckCategoryRoleRadioButtonId())
+        editText.setText(data.getString(UserInfo.FIELD_DISPLAY_NAME, ""), TextView.BufferType.NORMAL)
+        editText2.setText(data.getString(UserInfo.FIELD_TWITTER_ID, ""), TextView.BufferType.NORMAL)
+        which_charge.check(getCheckChargeRadioButtonId())
+        region_spinner.setSelection(data.getInt(UserInfo.FIELD_REGION, 1))
+        sex_spinner.setSelection(data.getInt(UserInfo.FIELD_SEX, 1))
+        age_spinner.setSelection(data.getInt(UserInfo.FIELD_AGE, 1))
+        imagination_hope.setText(data.getString(UserInfo.FIELD_PHOTO_IMAGE, ""), TextView.BufferType.NORMAL)
+    }
     override fun onAttach(context: Context) {
         super.onAttach(context)
     }
 
     override fun onDetach() {
         super.onDetach()
+    }
+
+    private fun getCheckCategoryRoleRadioButtonId(): Int {
+        if (0 == data.getInt(UserInfo.FIELD_CATEGORY_ROLE, 1)) {
+            return R.id.category_role_cameraman_edit
+        } else if(1 == data.getInt(UserInfo.FIELD_CATEGORY_ROLE, 1)){
+            return R.id.category_role_model_edit
+        } else {
+            return R.id.category_role_none_edit
+        }
+    }
+
+    private fun getCheckChargeRadioButtonId(): Int {
+        if (0 == data.getInt(UserInfo.FIELD_CHARGE, 1)) {
+            return R.id.no_charge_edit
+        } else if(1 == data.getInt(UserInfo.FIELD_CHARGE, 1)) {
+            return R.id.charge_edit
+        } else {
+            return R.id.charge_either_edit
+        }
     }
 }
