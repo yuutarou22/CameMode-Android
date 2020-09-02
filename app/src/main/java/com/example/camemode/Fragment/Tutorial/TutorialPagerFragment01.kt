d7674e168c0560bc2a3e168be5de8a7a696372a3
YutaroM <yutaroapp@gmail.com>
2020-09-02 21:52:28 +0900

    チュートリアル画面のFragmentを追加

---
+++ app/src/main/java/com/example/camemode/Fragment/Tutorial/TutorialPagerFragment01.kt
@@ -0,0 +1,19 @@
+package com.example.camemode.Fragment.Tutorial
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.camemode.R
+
+class TutorialPagerFragment01 : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_tutorial_pager_fragment01, container, false)
+    }
+
+}
