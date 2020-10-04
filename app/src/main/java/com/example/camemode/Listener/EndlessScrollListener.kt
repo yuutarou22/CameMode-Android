9b7ac72825e3c1564e67705ad7294d8f8eed88b8
YutaroM <yutaroapp@gmail.com>
2020-10-05 07:39:19 +0900

    スクロール最下部時を検知するリスナーを追加

--- app/src/main/java/com/example/camemode/Listener/EndlessScrollListener.kt
+++ app/src/main/java/com/example/camemode/Listener/EndlessScrollListener.kt
@@ -1,8 +1,16 @@
 package com.example.camemode.Listener
 
+import android.content.Context
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.camemode.Model.UserInfoModel
+import com.example.camemode.Task.DisplayUtil
+import com.example.camemode.Task.SearchUtil
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class EndlessScrollListener: RecyclerView.OnScrollListener() {
+class EndlessScrollListener(context: Context): RecyclerView.OnScrollListener(), SearchUtil.SearchedListener{
+
+    val context = context
 
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         super.onScrolled(recyclerView, dx, dy)
@@ -15,6 +23,20 @@ class EndlessScrollListener: RecyclerView.OnScrollListener() {
         if (!recyclerView.canScrollVertically(1)) {
             // リストの末尾の場合
             android.util.Log.d("EndlessScrollListener", "onScrolled Bottom")
+
+            // 取得したユーザ情報の最古情報を取得する
+            val searchUtil = SearchUtil()
+            searchUtil.setSearchLitener(this)
+            searchUtil.searchUserInfoAuto()
         }
+    }
+
+    override fun onSuccess(list: ArrayList<UserInfoModel>) {
+        android.util.Log.d("EndlessScrollListener", "onSuccess")
+
+        var displayUtil = DisplayUtil()
+
+        (context as FragmentActivity).supportFragmentManager?.let { displayUtil.displayUserInfo(list, it, context, context.user_info_list_home) }
+        context.loading_indicator.visibility = android.widget.ProgressBar.INVISIBLE
     }
 }
